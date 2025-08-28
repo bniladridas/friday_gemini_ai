aae0ec26b7eae31b589b4f4647a9214864f6b0a7
bniladridas <bniladridas@users.noreply.github.com>
2025-08-28 17:09:11 +0530

    test: update test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -304,8 +304,8 @@ module Minitest
   end
 
   # Helper method to create a test client
-  def create_test_client(model: :pro, **options)
-    GeminiAI::Client.new('test_key', model: model, **options)
+  def create_test_client(model: :pro, **)
+    GeminiAI::Client.new('test_key', model:, **)
   end
 end
 
