77abd419e44dbdf70c5e6e9dfa9e8fdf5cd21412
bniladridas <bniladridas@users.noreply.github.com>
2025-10-07 02:59:29 +0530

    Remove minitest-reporters to fix bundler issues with ruby-progressbar

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -72,12 +72,12 @@ end
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'gemini'
 require 'minitest/autorun'
-require 'minitest/reporters'
+# require 'minitest/reporters'
 require 'mocha/minitest'
 require 'stringio'
 
 # Configure Minitest
-Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+# Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # Add helper methods to Minitest::Test
 module Minitest
